e38f2680de7f6a013a48faed3594e94457afc8bd
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-06-14 20:23:38 -0600

    Created MVC back end skeleton which required some movement of appjs indexjs files

---
+++ server/src/controller/food.js
@@ -0,0 +1,49 @@
+const model = require('../model/food.js')
+
+function userLogin () {
+
+}
+
+function userSignup () {
+
+}
+
+function userInfo () {
+
+}
+
+function addIngredient () {
+
+}
+
+function deleteIngredient () {
+
+}
+
+function createEvent () {
+
+}
+
+function eventInfo () {
+
+}
+
+function updateEvent () {
+
+}
+
+function getRecipe () {
+
+}
+
+module.exports = {
+  userLogin,
+  userSignup,
+  userInfo,
+  addIngredient,
+  deleteIngredient,
+  createEvent,
+  eventInfo,
+  updateEvent,
+  getRecipe
+}
