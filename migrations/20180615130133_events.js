5852a892e5af8ff53e7d9d3f9e3fb628ee7af1bd
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-06-15 13:25:26 -0600

    created all knex migrations

---
+++ migrations/20180615130133_events.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('events', table => {
+    table.increments()
+    table.string('location').notNullable().defaultsTo('')
+    table.dateTime('date_time').notNullable().defaultsTo(knex.fn.now())
+    table.integer('host_id').notNulalble()
+    table.foreign('host_id').references('users.id').onDelete('CASCADE')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable()
+};
