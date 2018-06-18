016869699fd1f12b7f0f57e351b657f78359cf34
rstatz <robinstatz@gmail.com>
2018-06-18 16:26:27 -0600

    last push of the day. Still a mess.

---
+++ public/events.js
@@ -0,0 +1,9 @@
+$( document ).ready(function(){
+  let body = document.getElementsByClassName('main')
+  console.log(body)
+
+  .addEventListener("click", function(e) {
+    e.preventDefault();
+
+  })
+})
