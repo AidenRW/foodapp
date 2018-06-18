8914cc65ba0f44906744dc37a96e4a24b5baea7d
rstatz <robinstatz@gmail.com>
2018-06-17 19:25:20 -0600

    committing before I take a break.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 $( document ).ready(function(){
+  $(".button-collapse").sideNav();
 
-$(".button-collapse").sideNav();
+let name = document.GetElementById('name')
+let email = document.GetElementById('email')
+let password = document.getElementById('password')
+let launch = document.getElementById('launch')
+
+launch.addEventListener('touch', function(e) {
+  e.preventDefault();
+
+
+
+
+  // setTimeout(function() {
+  //   document.location.href = 'pig-map.html'
+  // }, 2500)
+})
 
 })
