53fe0cee779264be10873c2bbe0e3177bc997e14
rstatz <robinstatz@gmail.com>
2018-06-18 09:11:58 -0600

    workiing on linking the front end to the back.

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
 
-let name = document.GetElementById('name')
-let email = document.GetElementById('email')
-let password = document.getElementById('password')
-let launch = document.getElementById('launch')
+let name = document.getElementById("name")
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let launch = document.getElementById("launch")
 
-launch.addEventListener('touch', function(e) {
+launch.addEventListener("click", function(e) {
   e.preventDefault();
 
+  //need to get the input info from the login form and send data to database.
+
+  localStorage.setItem("nameToStore", JSON.stringify(name.value))
+
+console.log(e)
+console.log(name.value)
+
+  $.ajax({
+    type: "POST",
+    url : `http://localhost:3000`,
+    data: {nameToStore, emailToStore, passwordToStore}
+  })
+  // .then(result => <insert result>)
 
 
 
