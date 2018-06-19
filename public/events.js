ad7b4b86fc090dc324487cef521af7cca87ec99b
rstatz <robinstatz@gmail.com>
2018-06-19 10:01:25 -0600

    changed the logo so that it will reflect the actual font we chose.

--- public/events.js
+++ public/events.js
@@ -1,30 +1,50 @@
 $( document ).ready(function(){
-  let createEvent = document.getElementById('create-event')
-  let main = document.getElementById('events-box')
+
   // // console.log(join)
+  let createEvent = document.getElementById('create-event')
+  let node = document.getElementById('events-box')
 
   createEvent.addEventListener("click", function(e) {
     e.preventDefault()
+    // console.log(node)
+    let createEventsForm = document.createElement(form)
+    let nameLabel = document.createElement(label)
+    let nameInput = document.createElement(input)
+    let timeLabel = document.createElement(label)
+    let timeInput = document.createElement(input)
+    let dateLabel = document.createElement(label)
+    let dateInput = document.createElement(input)
+    let locationLabel = document.createElement(label)
+    let locationInput = document.createElement(input)
 
-    console.log("click occurred on", e.target);
+    createEventsForm.appendChild(nameLabel)
+    createEventsForm.appendChild(nameInput)
+    createEventsForm.appendChild(timeLabel)
+    createEventsForm.appendChild(nameInput)
+    createEventsForm.appendChild(dateLabel)
+    createEventsForm.appendChild(dateInput)
+    createEventsForm.appendChild(locationLabel)
+    createEventsForm.appendChild(locationInput)
 
-    let createForm =
-    `<form id="events-form">
-      <div class="events-form-group">
-        <label for="event-name">EVENT NAME</label>
-        <input name="name" type="text" class="form-control" id="name" required>
-        <label for="time">TIME</label>
-        <input name="time" type="text" class="form-control" id="time">
-        <label for="date">DATE</label>
-        <input name="date" type="text" class="form-control" id="date">
-        <label for="where">WHERE</label>
-        <input name="where" type="text" class="form-control" id="where">
-      </div>
-    </form>`
+    node.appendChild(createEventsForm)
 
+    // `<form id="events-form">
+    //   <div class="events-form-group">
+    //     <label for="event-name">EVENT NAME</label>
+    //     <input name="name" type="text" class="form-control" id="name" required>
+    //     <label for="time">TIME</label>
+    //     <input name="time" type="text" class="form-control" id="time">
+    //     <label for="date">DATE</label>
+    //     <input name="date" type="text" class="form-control" id="date">
+    //     <label for="where">WHERE</label>
+    //     <input name="where" type="text" class="form-control" id="where">
+    //     <button type="submit" class="btn" id="create-btn">CREATE EVENT</button>
+    //   </div>
+    // </form>`
+// console.log(createForm)
     // let formSpot = document.createElement("div")
-    main.appendChild(createForm)
-
+    node.appendChild(createForm)
+// console.log("click occurred on", e.target);
 
 
   })
