1fbc8eb4a2002761d15dad6e1aab43b075b4889e
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-06-21 15:35:37 -0600

    fixed seed files

--- seeds/03_events.js
+++ seeds/03_events.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('events').insert([
-        {id: 1, event_name: "Ben's BBQ", location: 'Downtown', date: '2018-06-24', time: '15:45:00', host_id: 1},
-        {id: 2, event_name: "Robin's Luau", location: 'Roof', date: '2018-06-24', time: '12:45:00', host_id: 2},
-        {id: 3, event_name: "Aiden's Karaoke Party", location: 'Pool', date: '2018-06-24', time: '17:45:00', host_id: 3}
+        {id: 1, event_name: "Ben's BBQ", location: 'Pool', date: '2018-06-23', time: '12:00:00', host_id: 1},
+        {id: 2, event_name: "Robin's Luau", location: 'Roof', date: '2018-06-24', time: '18:00:00', host_id: 2},
+        {id: 3, event_name: "Aiden's Karaoke Party", location: 'Downtown', date: '2018-06-25', time: '20:00:00', host_id: 3}
       ])
     }).then(() => {
       return knex.raw(
